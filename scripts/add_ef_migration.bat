eb162f5a2e7bd9f4454827059e16a5e746da12a4
Jakub Petrylak <jakub.petrylak@gmail.com>
2023-12-10 20:23:39 +0100

    Delete scripts folder

--- scripts/add_ef_migration.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-dotnet ef migrations add %1 ^
--c eShop.Infrastructure.EntityFramework.EShopDbContext ^
--o EntityFramework\Migrations ^
--p ..\Source\eShop.Infrastructure\eShop.Infrastructure.csproj ^
--s ..\Source\eShop.WebApi\eShop.WebApi.csproj ^
