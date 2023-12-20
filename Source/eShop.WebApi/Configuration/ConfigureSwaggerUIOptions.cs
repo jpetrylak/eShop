f8cc30d61bc637e0ae19ea018ca31891449c5edb
Jakub Petrylak <jakubpetrylak@gmail.com>
2023-12-20 23:55:07 +0100

    Display environment name in swagger document title (in browser).

--- Source/eShop.WebApi/Configuration/ConfigureSwaggerUIOptions.cs
+++ Source/eShop.WebApi/Configuration/ConfigureSwaggerUIOptions.cs
@@ -3,12 +3,12 @@ using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace eShop.Configuration;
 
-public class ConfigureSwaggerUIOptions()
+public class ConfigureSwaggerUIOptions(IWebHostEnvironment environment)
     : IConfigureOptions<SwaggerUIOptions>
 {
     public void Configure(SwaggerUIOptions options)
     {
-        options.DocumentTitle = "eShop";
+        options.DocumentTitle = $"eShop {environment.EnvironmentName}";
         options.DisplayOperationId();
         options.DisplayRequestDuration();
     }
