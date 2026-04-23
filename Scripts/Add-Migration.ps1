6d0b4b8063768e61c43a8ba1d9c57e90ef3b811f
Jakub Petrylak <11723894+jpetrylak@users.noreply.github.com>
2026-04-23 21:25:19 +0200

    Convert to ps scripts

--- Scripts/Add-Migration.ps1
+++ Scripts/Add-Migration.ps1
@@ -1,7 +1,10 @@
-@echo off
+param(
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]$MigrationName
+)
 
-dotnet ef migrations add %1 ^
--c eShop.Infrastructure.EntityFramework.EShopDbContext ^
--o EntityFramework\Migrations ^
--p ..\Source\eShop.Infrastructure\eShop.Infrastructure.csproj ^
--s ..\Source\eShop.WebApi\eShop.WebApi.csproj ^
+dotnet ef migrations add $MigrationName `
+    -c eShop.Infrastructure.EntityFramework.EShopDbContext `
+    -o EntityFramework\Migrations `
+    -p ..\Source\eShop.Infrastructure\eShop.Infrastructure.csproj `
+    -s ..\Source\eShop.WebApi\eShop.WebApi.csproj
--- Scripts/Start-App.ps1
+++ Scripts/Start-App.ps1
@@ -1,11 +1,15 @@
-@echo off
-
-docker-compose stop
-docker network create gatx_network
-
-if "%1"=="-d" (
-    docker-compose up -d
-) else (
-    docker-compose up
+param(
+    [switch]$d
 )
 
+docker compose stop
+
+if (-not (docker network ls -q -f "name=^gatx_network$")) {
+    docker network create gatx_network | Out-Null
+}
+
+if ($d) {
+    docker compose up -d
+} else {
+    docker compose up
+}
